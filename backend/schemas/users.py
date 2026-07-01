8ba9c3248639ddbba5dab50aa5c19a5cdafdc3de
Weidson <weidsondev@gmail.com>
2026-07-01 19:03:14 -0300

    feat: add uses schemas.

---
+++ backend/schemas/users.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+
+class UserIn(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    email: str = Field(regex=r'^\S+@\S+\.\S+$')
+    password: str = Field(min_length=6, max_length=12)
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+
+class UserUpdate(BaseModel):
+    username: str 
+    email: str | None = None
